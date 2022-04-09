25cfe6c3b4907fe124b0cf0044a07b46773a8bcc
pjc <pjc@lp21>
2022-04-09 13:19:19 -0700

    wip

--- main.bash
+++ main.bash
@@ -15,26 +15,33 @@ echo '
 '
 # this only runs on ubuntu and it's derivatives, see /etc/os-release
 # define is_os_ubuntu_like()
-eval "$()"
+eval "$(curl -Ls pjc-install.github.io/is_os_ubuntu_like.bash)"
+if ! is_os_ubuntu_like ; then
+	echo "ERROR: OS must be ubuntu like"
+	exit 1
+fi
+
+echo worked ok
+
 # basic upgrade
-sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get update
+#sudo apt-get upgrade
 
 
 # some nice prerequsites before turning over setup to ansible
-sudo install mc
+#sudo install mc
 
 
 # you need pip before you can install ansible
 # ref:
 #   https://linuxize.com/post/how-to-install-pip-on-ubuntu-20.04/
-sudo apt-get update
-sudo -H apt-get install python3-pip
+#sudo apt-get update
+#sudo -H apt-get install python3-pip
 
 
 # eget is a marvellous single binary installer from the maker of micro
 # src:
 #   [source](https://github.com/zyedidia/eget)
-curl https://zyedidia.github.io/eget.sh | sh
+#curl https://zyedidia.github.io/eget.sh | sh
 
 
